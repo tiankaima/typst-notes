06117f94e6f48aac679025e72340b992562152e7
TianKai Ma <tiankaima@163.com>
2024-09-17 02:03:46 +0800

    feat: add 2024 LUG SFD Poster

--- setup-font.sh
+++ setup-font.sh
@@ -1,4 +1,6 @@
 wget https://github.com/adobe-fonts/source-han-serif/releases/download/2.002R/01_SourceHanSerif.ttc.zip
+wget https://github.com/tiankaima/typst-notes/releases/download/24_SP_TA%2Fv1/extras.zip
 mkdir -p fonts
 unzip 01_SourceHanSerif.ttc.zip -d fonts
+unzip extras.zip -d fonts
 rm 01_SourceHanSerif.ttc.zip
